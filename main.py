9d36c721e1c6ffe9235c68841355dc5e4b67db24
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-10 13:31:16 -0500

    refactor: refactor hello world to welcome message

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello World!"}
+@app.get("/", tags=['Welcome'], response_description='Displays welcome message')
+async def welcome(repsonse: Response):
+    repsonse.status_code = status.HTTP_200_OK
+    return {"message": "Hello, welcome to Justice Leagues FastAPI"}
 
 
 if __name__ == "__main__":
